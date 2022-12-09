a86948fb9da0357cdaefa34f2d79af47f24d6aec
David Provencher <david.provencher@avidbots.com>
2022-12-09 00:05:41 -0500

    Refactoring (day8)

--- src/day8.py
+++ src/day8.py
@@ -1,10 +1,47 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Callable, List
+
 import numpy as np
+from numpy.lib.index_tricks import IndexExpression
 
 import utils.io
 
 
+# Utils for applying 1D processing function along multiple directions on a 2D array
+@dataclass
+class ArrayView1D:
+    axis: int
+    s_: IndexExpression = np.s_[:, :]
+
+
+class Direction(Enum):
+    Top = ArrayView1D(axis=0)
+    Bottom = ArrayView1D(axis=0, s_=np.s_[::-1, :])
+    Left = ArrayView1D(axis=1)
+    Right = ArrayView1D(axis=1, s_=np.s_[:, ::-1])
+
+
+def apply_along_directions(
+    x: np.ndarray, func: Callable, directions: List[Direction], aggregate_func: Callable
+):
+    if not directions:
+        raise ValueError("No directions to apply")
+
+    view: ArrayView1D = directions[0].value
+    result = np.apply_along_axis(func, view.axis, forest[view.s_])[view.s_]
+
+    for dir in directions[1:]:
+        view = dir.value
+        result = aggregate_func(
+            result, np.apply_along_axis(func, view.axis, forest[view.s_])[view.s_]
+        )
+
+    return result
+
+
+# 1D processing functions
 def is_visible_1D(line_of_trees):
-    # Works on 1D array
     is_visible = np.zeros(line_of_trees.shape, dtype=bool)
 
     # First tree is visible
@@ -38,35 +75,33 @@ def n_trees_in_view_1D(line_of_trees):
     return trees_in_view
 
 
-def is_visible_from_outside(forest):
-    flip_0 = np.s_[::-1, :]
-    flip_1 = np.s_[:, ::-1]
-
-    return (
-        np.apply_along_axis(is_visible_1D, 0, forest)  # From top
-        + np.apply_along_axis(is_visible_1D, 0, forest[flip_0])[flip_0]  # From bottom
-        + np.apply_along_axis(is_visible_1D, 1, forest)  # From left
-        + np.apply_along_axis(is_visible_1D, 1, forest[flip_1])[flip_1]  # From right
+# Functions for solving
+def count_trees_visible_from_outside(forest):
+    is_visible = apply_along_directions(
+        forest,
+        func=is_visible_1D,
+        directions=[Direction.Top, Direction.Bottom, Direction.Left, Direction.Right],
+        aggregate_func=np.add,
     )
 
+    return np.count_nonzero(is_visible)
 
-def scenic_score(forest):
-    flip_0 = np.s_[::-1, :]
-    flip_1 = np.s_[:, ::-1]
 
-    return (
-        np.apply_along_axis(n_trees_in_view_1D, 0, forest)  # From top
-        * np.apply_along_axis(n_trees_in_view_1D, 0, forest[flip_0])[flip_0]  # From bottom
-        * np.apply_along_axis(n_trees_in_view_1D, 1, forest)  # From left
-        * np.apply_along_axis(n_trees_in_view_1D, 1, forest[flip_1])[flip_1]  # From right
+def max_scenic_score(forest):
+    scenic_score = apply_along_directions(
+        forest,
+        func=n_trees_in_view_1D,
+        directions=[Direction.Top, Direction.Bottom, Direction.Left, Direction.Right],
+        aggregate_func=np.multiply,
     )
+    return np.amax(scenic_score)
 
 
 if __name__ == "__main__":
     forest = utils.io.read_file_as_array("input/day8", int)
 
     print("Part I")
-    print(np.count_nonzero(is_visible_from_outside(forest)))
+    print(count_trees_visible_from_outside(forest))
 
     print("Part II")
-    print(np.amax(scenic_score(forest)))
+    print(max_scenic_score(forest))
