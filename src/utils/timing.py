8799cd6bda8ebfbcc1ee101963ad7a65c38fe4f1
David Provencher <david.provencher@avidbots.com>
2022-12-21 00:23:47 -0500

    Solve day16

--- src/utils/timing.py
+++ src/utils/timing.py
@@ -10,8 +10,8 @@ def timing(f):
         ts = time.perf_counter()
         result = f(*args, **kw)
         te = time.perf_counter()
-        if te - ts > 0.0001:
-            print(f"Function {f.__name__} took {te-ts:2.4f} seconds")
+        # if te - ts > 0.0001:
+        print(f"Function {f.__name__} took {te-ts:2.4f} seconds")
         return result
 
     return wrap
