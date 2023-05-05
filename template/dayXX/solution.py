e7a266f400102a9352a2b559c29357c701479d0d
David Provencher <dprovencher85@gmail.com>
2023-05-04 22:33:32 -0400

    Added template for solving new day

---
+++ template/dayXX/solution.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+import utils.io
+
+
+###########################################
+# Parsing utils
+###########################################
+def parse_data_as_XX(input_file: str):
+    return None
+
+
+###########################################
+# Solvers
+###########################################
+def solve_part_1(input_file: str) -> int:
+    data = parse_data_as_XX(input_file)
+    return None
+
+
+def solve_part_2(input_file: str) -> int:
+    data = parse_data_as_XX(input_file)
+    return None
+
+
+if __name__ == "__main__":
+    print("Part I")
+    print(solve_part_1("input/dayXX.txt"))
+
+    print("Part II")
+    print(solve_part_2("input/dayXX.txt"))
---
+++ template/dayXX/test_solution.py
@@ -0,0 +1,12 @@
+import utils.test
+
+
+class TestSolution(utils.test.TestSolutionTemplate):
+    def setup_class(cls):
+        cls.day: int = 0
+
+        cls.part_1_example_result = None
+        cls.part_2_example_result = None
+
+        cls.part_1_result = None
+        cls.part_2_result = None
